5d8975be584c7a7abd1dcbb63552f68c31c6c701
Arnold120 <arnold.logo23904156@estu.unan.edu.ni>
2025-10-29 19:48:07 -0600

    Animaciones del ejercicio de palindromo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(home: Palindromo()));
 
@@ -7,11 +8,38 @@ class Palindromo extends StatefulWidget {
   State<Palindromo> createState() => _PalindromoState();
 }
 
-class _PalindromoState extends State<Palindromo> {
+class _PalindromoState extends State<Palindromo> with SingleTickerProviderStateMixin {
   final c = TextEditingController(); String r = '';
+  Color color = Colors.black;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+    _animation = Tween<double>(begin: 0, end: 8)
+        .chain(CurveTween(curve: Curves.elasticIn))
+        .animate(_controller);
+  }
+
   void check() {
     var t = c.text.toLowerCase().replaceAll(' ', '');
-    setState(() => r = t == t.split('').reversed.join() ? 'Palíndromo' : 'No lo es');
+    bool esPalindromo = t == t.split('').reversed.join();
+    setState(() {
+      r = esPalindromo ? 'Es palindromo' : 'No es palindromo';
+      color = esPalindromo ? Colors.green : Colors.red;
+    });
+    if (!esPalindromo) _controller.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,7 +50,17 @@ class _PalindromoState extends State<Palindromo> {
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         TextField(controller: c, decoration: InputDecoration(labelText: 'Palabra')),
         ElevatedButton (onPressed: check, child: Text('Verificar')),
-        Text(r, style: TextStyle(fontSize: 20))
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) => Transform.translate(
+            offset: Offset(sin(_animation.value) * 8, 0),
+            child: AnimatedDefaultTextStyle(
+              duration: Duration(milliseconds: 300),
+              style: TextStyle(fontSize: 20, color: color),
+              child: Text(r),
+            ),
+          ),
+        ),
       ]),
     ),
   );
